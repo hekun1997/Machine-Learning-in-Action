e052fdac5b97984f9bfb2d9501eccbbfc4706fd3
hekun1997 <48910648+hekun1997@users.noreply.github.com>
2021-04-26 23:28:39 +0800

    add decision-tree and kNN, add weekly plan and monthly plan.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Machine-Learning-in-Action
-python version-3.8
+python version-2.7
+
+##2021年
+
+4月26日-5月15日，学习《机器学习实战》中的相关内容，并使用python实现相关代码。
+
+周计划：
+4月26日-5月2日，学习《机器学习实战》中 kNN，决策树，朴素贝叶斯，Logistic回归，支持向量机相关概念以及使用Python实现。
+
+5月3日-5月9日，学习《机器学习实战》中AdaBoost算法，回归算法，树回归相关及实现。
+
+5月10日-5月15日，学习《机器学习实战》中 ，k均值聚类，Apriori算法，FP-growth算法。
--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,56 @@
-import kNN.kNN as knn
-from numpy import *
-import operator
+# -*- coding: UTF-8 -*-
 
+import requests as r
+import random
+import json
+
+
+def getMidStr(txt, txt_start, txt_end='', seeks=0, seeke=0):
+    try:
+        if txt_end or seeks or seeke:
+            pass
+        else:
+            raise 1
+        s_1 = txt.find(txt_start)
+        if s_1 == -1:
+            raise 1
+        l_1 = len(txt_start)
+        if txt_end:
+            s_2 = txt.find(txt_end, s_1)
+            if s_1 == -1 or s_2 == -1:
+                return False
+            return txt[s_1 + l_1:s_2]
+        if seeks:
+            return txt[s_1 - seeks:s_1]
+        if seeke:
+            return txt[s_1 + l_1:s_1 + l_1 + seeke]
+    except Exception:
+        return '传参错误或未找到传参文本'
+
+
+# while True:
 if __name__ == '__main__':
-    pass
+    while True:
+        hero = input("输入需要查询的英雄:")
+        if hero == "":
+            print("您输入无效")
+        elif hero == "0":
+            exit()
+        else:
+            source = r.get("http://s.wukongfenshen.com:9972/main/api/honour/wzpower/static/js/index.js?v=0." + str(
+                random.randint(1000000, 9999999))).text
+            _json = getMidStr(source, "var dictAll = ", "}}") + "}}"
+            _json = json.loads(_json)
+            try:
+                _json[hero]
+            except KeyError as reason:
+                print("没有英雄:%s" % reason)
+                continue
+            title = _json[hero]["title"]
+            content = _json[hero]["content"]
+            print("英雄:%s" % title)
+            content = str.replace(content, "<p><p>", "\n")
+            content = str.replace(content, "<p> <p>", "\n")
+            content = str.replace(content, "</p>", "\n")
+            content = str.replace(content, "<p>", "")
+            print(content + "\n")
